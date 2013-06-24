571a2f83910f10702866f644c1c9ae6dc9ec888a
gnowland <gnowland@gmail.com>
2013-06-24 05:29:07 -0500

    added back logo anim js

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ var newsize = function(){
 };
 
 $(document).ready(newsize);
-//$(window).load(newsize);  <DOCUMENT READY HAPPENS SOONER
+//$(window).load(newsize);  DOCUMENT READY HAPPENS SOONER
 $(window).resize(newsize);
 
 
 
-//ANIMATION
+//BOX ANIMATION
 
 $(window).load(function(){
 $("body").delegate('.box','click', function(){
@@ -77,4 +77,19 @@ $("body").delegate('.box.active','click', function(){
     
 });
 
+});
+
+
+
+//LOGO ANIMATION
+
+$(window).on('mousemove', function(event) {
+    var width = $(window).width();
+    var mouseX = event.pageX - (width * 0.5);
+    var height = $(window).height();
+    var mouseY = event.pageY - (height * 0.5);
+    var xAngle = (mouseY / height) * 90;
+    var yAngle = (mouseX / width) * 90;
+
+    $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 });

016c1f2fffcd2b77ffe3b463034ea7b966ea9a34
gnowland <gnowland@gmail.com>
2013-06-24 17:27:38 -0500

    removed cube

--- script.js
+++ script.js
@@ -78,18 +78,3 @@ $("body").delegate('.box.active','click', function(){
 });
 
 });
-
-
-
-//LOGO ANIMATION
-
-$(window).on('mousemove', function(event) {
-    var width = $(window).width();
-    var mouseX = event.pageX - (width * 0.5);
-    var height = $(window).height();
-    var mouseY = event.pageY - (height * 0.5);
-    var xAngle = (mouseY / height) * 90;
-    var yAngle = (mouseX / width) * 90;
-
-    $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
-});

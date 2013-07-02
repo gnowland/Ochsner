340cc29053f1ee6116c2efdb8d7416685ae3d803
gnowland <gnowland@gmail.com>
2013-07-01 19:02:44 -0500

    forget it with the scrollbars. maybe later

--- script.js
+++ script.js
@@ -81,16 +81,9 @@ $('#contentwindow').delegate('.box','click', function(){
 			}); 
 			
 			//SHADOWBOX FIXING
-			$('.box.inactive .galleryboxInner a').attr('rel', 'noshadowbox')			
+			$('.box.inactive ul.gallery.fade li a').attr('rel', 'noshadowbox')			
 			Shadowbox.clearCache();
 			Shadowbox.setup();
-			
-			
-			//GALLERY FIXING
-			var picnum = $('.active .scroller li').length;
-			var scrollerw = 200 * picnum;
-			
-			$('ul.scroller').css({'width': scrollerw + 'px'});
 			
     } //CLOSE ifboxactive
 }); //CLOSE window load

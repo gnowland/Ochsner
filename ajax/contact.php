3458acf946cf67e85e6dd7b07a374380dcaf798d
gnowland <gnowland@gmail.com>
2013-09-16 02:14:34 -0500

    start folder migration

---
+++ ajax/contact.php
@@ -0,0 +1,34 @@
+<div class="box zero onloadfadein cloned active" data-src="img/boxes/contact.jpg" style="left: 0px; top: 0px; width: 100%; height: 91.972049689441%; padding: 64.625px 0px 0px; margin: 0px; overflow: auto;"><!--BOX 1 (WHITEHALL)-->
+                <div id="leftcol" style="width: 23%;">
+                    <div class="boximage" style="height: 179.2px; width: 70%; margin: 5% 0px -45% 15%; padding: 0px;"></div>
+                    <div class="sprite fade">
+                        <div class="non-semantic-protector"><h1 class="ribbon"><strong class="ribbon-content">contact me</strong></h1></div>
+                        <ul style=" height:530px; text-align:center;">
+                        <li style="width:94%;display:block; padding-bottom:430px; background-image:none;">
+<a id="foxyform_embed_link_443030" href="http://www.foxyform.com/"></a>
+<script type="text/javascript">
+(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://www.foxyform.com/js.php?id=443030&sec_hash=e5185ad1381&width=100%25";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
+</script></li>
+						<li><span style="color:#FFF">Join me on LinkedIn:</span><br /><a href="http://www.linkedin.com/in/giffordnowland" target="_blank"><img src="../img/linkedin.png" width="152" height="50" border="0" alt="View Gifford Nowland's profile on LinkedIn" style="background-color:#fff;"></a></li>
+                        </ul>
+                     </div>
+            	 </div>
+                   <div id="rightcol" style="margin: 0px auto; text-align: center; width: 76.46852646638055%;">		
+                     <div class="content fade">
+                         <div class="title" style="padding-top:50px; text-align:center;">
+                             <!--<h1 style="text-align:center;">Gifford R. Nowland</h1>-->
+                             <h4 style="padding-left:0;">Résumé</h4>
+                             <div class="timeline" style="margin-top:-15px!important;">Updated: June 2013</div>
+                         </div>
+                         <iframe class="iframe" src="http://docs.google.com/viewer?url=http%3A%2F%2Fwww.giffordnowland.com%2Fochsner%2FGiffordR.Nowland-Resume.pdf&embedded=true" height="1040" style="border: none;"></iframe>
+                     </div>
+                </div>
+        	</div>
+    	</div>
+	</div><!--END BOX-->
+

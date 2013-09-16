49a9ce76c7d85c35a19cf3f1c963958fc7fc569c
gnowland <gnowland@gmail.com>
2013-09-16 01:51:19 -0500

    branched window from script.js

--- ajax.js
+++ ajax.js
@@ -1,5 +1,13 @@
 // JavaScript Document
 
 $.ajaxSetup ({  
-    cache: false  
+    cache: false  //change this later
 });  
+
+var ajax_load = "<img src='img/ajax_loader_blue_48.gif' alt='loading...' />";  
+      
+//  load() functions  
+    var loadUrl = "ajax/load.php";  
+    $("#load_basic").click(function(){  
+        $("#result").html(ajax_load).load(loadUrl);  
+    });
---
+++ windowresize.js
@@ -0,0 +1,49 @@
+// JavaScript Document
+
+//WINDOW RESIZE
+
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null; 
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100); 
+      };
+  }
+    // smartresize 
+    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+
+var newsize = function(){
+	var b = $('#contentwindow').offset().top;
+	var y = $(window).height();
+	var z = $('#contentwindow').outerHeight(true);
+	var h = $('#contentwindow').height();
+	var w = z - h;
+	var a = y - b;
+	var e = a - w;
+	
+	$('#contentwindow').css({'height': e + 'px'});
+};
+
+// usage:
+$(window).smartresize(newsize);
+$(document).ready(newsize);

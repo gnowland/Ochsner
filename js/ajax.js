617c639328895654cfd9e3f3f9cab4b20086d3fd
gnowland <gnowland@gmail.com>
2013-09-16 02:25:41 -0500

    updated file references

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,7 @@ $.ajaxSetup ({
     cache: false  //change this later
 });  
 
-var ajax_load = "<img src='img/ajax_loader_blue_48.gif' alt='loading...' />";  
+var ajax_load = "<img src='../img/ajax_loader_blue_48.gif' alt='loading...' />";  
       
 //  load() functions  
     var loadUrl = "ajax/load.php";  
